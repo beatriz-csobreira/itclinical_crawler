419a7fb0aa543ef8c7f50e6f4764d65eee3571c4
Beatriz Sobreira <beatriz.csobreira@gmail.com>
2024-12-15 19:50:53 +0000

    print bullet points in order of 'start_urls'

--- itclinical_crawler/itclinical_crawler/spiders/itclinical.py
+++ itclinical_crawler/itclinical_crawler/spiders/itclinical.py
@@ -10,9 +10,19 @@ class ItclinicalSpider(scrapy.Spider):
                   "https://itclinical.com/iwrs.php",
                   "https://itclinical.com/pharmacovigilance.php",
                   "https://itclinical.com/bibliovigilance.php"]
+    url_to_data = {}
+    response_count = 0
 
     def parse(self, response):
-        #according to the webpage, the bulletpoints are <ul> elements with the class "check-list"
+        # according to the webpage, the bulletpoints are <ul> elements with the class "check-list"
         bullet_points = response.css("ul.check-list li::text").getall()
-        for point in bullet_points:
-            print(point.strip())
+        self.url_to_data[response.url] = [s.strip() for s in bullet_points]
+
+        self.response_count += 1
+
+        # only print on the last response, the bullet points in order of 'start_urls'
+        if self.response_count == len(self.start_urls):
+            for url in self.start_urls:
+                print(url)
+                for point in self.url_to_data[url]:
+                    print(point)
