1e59675cf85565733e53806791e3f51b743d1839
Beatriz Sobreira <beatriz.csobreira@gmail.com>
2024-12-15 18:40:37 +0000

    extract the bulletpoints from each URL and print them unordered

--- itclinical_crawler/itclinical_crawler/spiders/itclinical.py
+++ itclinical_crawler/itclinical_crawler/spiders/itclinical.py
@@ -4,7 +4,15 @@ import scrapy
 class ItclinicalSpider(scrapy.Spider):
     name = "itclinical"
     allowed_domains = ["itclinical.com"]
-    start_urls = ["https://itclinical.com/it.php"]
+    start_urls = ["https://itclinical.com/ctms.php",
+                  "https://itclinical.com/edc.php",
+                  "https://itclinical.com/impo.php",
+                  "https://itclinical.com/iwrs.php",
+                  "https://itclinical.com/pharmacovigilance.php",
+                  "https://itclinical.com/bibliovigilance.php"]
 
     def parse(self, response):
-        print(response)
+        #according to the webpage, the bulletpoints are <ul> elements with the class "check-list"
+        bullet_points = response.css("ul.check-list li::text").getall()
+        for point in bullet_points:
+            print(point.strip())
