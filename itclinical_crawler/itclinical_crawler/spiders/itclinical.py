d826d17b4b7cd7280269b9b98a5e231d4debcfed
Beatriz Sobreira <beatriz.csobreira@gmail.com>
2024-12-15 20:13:14 +0000

    print name instead of url

--- itclinical_crawler/itclinical_crawler/spiders/itclinical.py
+++ itclinical_crawler/itclinical_crawler/spiders/itclinical.py
@@ -4,12 +4,24 @@ import scrapy
 class ItclinicalSpider(scrapy.Spider):
     name = "itclinical"
     allowed_domains = ["itclinical.com"]
-    start_urls = ["https://itclinical.com/ctms.php",
-                  "https://itclinical.com/edc.php",
-                  "https://itclinical.com/impo.php",
-                  "https://itclinical.com/iwrs.php",
-                  "https://itclinical.com/pharmacovigilance.php",
-                  "https://itclinical.com/bibliovigilance.php"]
+    start_urls = [
+        "https://itclinical.com/ctms.php",
+        "https://itclinical.com/edc.php",
+        "https://itclinical.com/impo.php",
+        "https://itclinical.com/iwrs.php",
+        "https://itclinical.com/pharmacovigilance.php",
+        "https://itclinical.com/bibliovigilance.php"
+    ]
+    
+    url_to_name = {
+        "https://itclinical.com/ctms.php": "CTMS",
+        "https://itclinical.com/edc.php": "EDC",
+        "https://itclinical.com/impo.php": "IMPO",
+        "https://itclinical.com/iwrs.php": "IRT:IWRS",
+        "https://itclinical.com/pharmacovigilance.php": "Pharmacovigilance",
+        "https://itclinical.com/bibliovigilance.php": "Bibliovigilance"
+    }
+
     url_to_data = {}
     response_count = 0
 
@@ -23,7 +35,7 @@ class ItclinicalSpider(scrapy.Spider):
         # only print on the last response, the bullet points in order of 'start_urls'
         if self.response_count == len(self.start_urls):
             for url in self.start_urls:
-                print(f"\n{url}\n")
+                print(f"\n{self.url_to_name[url]}\n")
                 for point in self.url_to_data[url]:
                     print(f"\t{point}")
             print()
